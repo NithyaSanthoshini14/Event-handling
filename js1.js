05a7f6366791a5e87a10e8395a1be3cc5223566d
NITHIN NITHYA <nithyagunda61@gmail.com>
2024-10-20 19:12:37 +0530

    pushing files

---
+++ js1.js
@@ -0,0 +1,40 @@
+function opacity1(){
+    let img=document.getElementsByClassName("div1")
+    img[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.1'>"
+}
+function opac2(){
+    let img1=document.getElementsByClassName("div1")
+    img1[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.2'>"
+}
+function opac3(){
+    let img2=document.getElementsByClassName("div1")
+    img2[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.3'>"
+}
+function opac4(){
+    let img3=document.getElementsByClassName("div1")
+    img3[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.4'>"
+}
+function opac5(){
+    let img4=document.getElementsByClassName("div1")
+    img4[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.5'>"
+}
+function opac6(){
+    let img5=document.getElementsByClassName("div1")
+    img5[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.6'>"
+}
+function opac7(){
+    let img6=document.getElementsByClassName("div1")
+    img6[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.7'>"
+}
+function opac8(){
+    let img7=document.getElementsByClassName("div1")
+    img7[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.8'>"
+}
+function opac9(){
+    let img8=document.getElementsByClassName("div1")
+    img8[0].innerHTML="<img src='./download (2).jpeg' style='opacity:0.9'>"
+}
+function opac10(){
+    let img9=document.getElementsByClassName("div1")
+    img9[0].innerHTML="<img src='./download (2).jpeg' style='opacity:1'>"
+}
